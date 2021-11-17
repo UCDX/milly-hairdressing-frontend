9751412811b80814ab60b9ed2cde3f3da7593055
RickJara <180300355@ucaribe.edu.mx>
2021-11-16 20:14:26 -0500

    Validación de que los campos estén llenos mediante notificación o alerta.

--- src/app/modules/views/register/register.component.ts
+++ src/app/modules/views/register/register.component.ts
@@ -14,6 +14,26 @@ export class RegisterComponent implements OnInit {
 
   registerBoxHandler(event: any) {
     console.log(event)
+    event.firstField = event.firstField.trim();
+    if(event.firstField == '') {
+      alert('El campo Nombre es requerido')
+      return;
+    }
+    event.secondField = event.secondField.trim();
+    if(event.secondField == '') {
+      alert('El campo Apellido es requerido')
+      return;
+    }
+    event.thirdField = event.thirdField.trim();
+    if(event.thirdField == '') {
+      alert('El campo Correo electrónico es requerido')
+      return;
+    }
+    event.fifthField = event.fifthField.trim();
+    if(event.fifthField == '') {
+      alert('El campo Contraseña es requerido')
+      return;
+    }
   }
 
 }
